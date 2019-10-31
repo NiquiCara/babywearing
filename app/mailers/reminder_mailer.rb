ec80051d95c6d0f5dc5238f38daee2fc488ecbac
Monique <monique.ae.cara@gmail.com>
2019-10-31 10:27:35 -0400

    Adds email for when due in a week

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -7,4 +7,13 @@ class ReminderMailer < ApplicationMailer
     @due_date = due_date
     mail(to: @user.email, subject: 'Baby Carrier Overdue For Return')
   end
+
+  def week_advance_notice_email(user, carrier_name, current_location, due_date)
+    @user = user
+    @first_name = user.full_name
+    @carrier_name = carrier_name
+    @current_location = current_location
+    @due_date = due_date
+    mail(to: @user.email, subject: 'Baby Carrier Due For Return')
+  end
 end
